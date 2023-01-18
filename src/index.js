8c7f54dbc7ec671632412a6a29d95056276a1dbc
PlatinumKellerman <platinumm@ukr.net>
2023-01-18 23:05:51 +0200

    start, install react app, theme

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
