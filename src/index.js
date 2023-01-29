70a033e72c46aea3e1ea8fb6eefe8393392a6fa6
PlatinumKellerman <platinumm@ukr.net>
2023-01-29 16:41:32 +0200

    in progress

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import GlobalStyle from './styles/globalStyles';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
       <GlobalStyle />
+      <BrowserRouter basename="/movie-viewer/">
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
