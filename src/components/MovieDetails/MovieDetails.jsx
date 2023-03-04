9529953242f584f60ba21e7b33ac9bb727ce64fe
PlatinumKellerman <platinumm@ukr.net>
2023-03-05 00:59:56 +0200

    in process

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,26 +1,61 @@
-import Container from 'layout/common/Container/Container';
-import { Poster } from './MovieDetails.styled';
+import {
+  Poster,
+  InfoParams,
+  MovieTitle,
+  InfoValue,
+} from './MovieDetails.styled';
 
 export const MovieDetails = ({ movie }) => {
+  const releaseDate = new Date(movie.release_date).getUTCFullYear();
+  console.log();
   console.log(movie);
   return (
-    <Container>
-      <Poster
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
-      ></Poster>
-      <p>{movie.title}</p>
-      <p>Budget:{movie.budget}</p>
-      <p>
-        Genres:
-        {movie.genres
-          ? movie.genres.map(genre => genre.name).join(', ')
-          : 'There are no genres!'}
-      </p>
-      <p>
-        Movie hompage: <a href={movie.homepage}>{movie.homepage}</a>
-      </p>
-      <p>Overview: {movie.overview}</p>
-    </Container>
+    <div>
+      <div>
+        <Poster
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        ></Poster>
+      </div>
+
+      <div>
+        <MovieTitle>
+          <InfoValue>
+            {movie.title} ({releaseDate})
+          </InfoValue>
+        </MovieTitle>
+        <InfoParams>
+          <InfoValue>Release Year: {releaseDate}</InfoValue>
+        </InfoParams>
+        <InfoParams>
+          User Score: {`${Number(movie.vote_average).toFixed(1) * 10}%`}
+        </InfoParams>
+        <InfoParams>
+          <InfoValue>
+            Rating: {movie.vote_average} ({movie.vote_count} votes)
+          </InfoValue>
+        </InfoParams>
+        <InfoParams>
+          <InfoValue>
+            {' '}
+            Genres:{' '}
+            {movie.genres
+              ? movie.genres.map(genre => genre.name).join(', ')
+              : 'There are no genres!'}
+          </InfoValue>
+        </InfoParams>
+        <InfoParams>
+          <InfoValue>Budget: ${movie.budget}</InfoValue>
+        </InfoParams>
+        <InfoParams>
+          <InfoValue>
+            Movie hompage: <a href={movie.homepage}>{movie.homepage}</a>
+          </InfoValue>
+        </InfoParams>
+        <InfoParams>
+          <InfoValue>Overview: {movie.overview}</InfoValue>
+        </InfoParams>
+      </div>
+    </div>
   );
 };
--- src/components/MovieDetails/MovieDetails.styled.js
+++ src/components/MovieDetails/MovieDetails.styled.js
@@ -3,3 +3,28 @@ import styled from 'styled-components';
 export const Poster = styled.img`
   width: auto;
 `;
+
+export const MovieTitle = styled.h3`
+  font-family: ${p => p.theme.fonts.body};
+  font-size: ${p => p.theme.fontSizes.l};
+  color: ${p => p.theme.colors.light};
+  text-shadow: ${p => p.theme.colors.grey} 1px 0px 0px,
+    ${p => p.theme.colors.dark} 0px 1px 0px,
+    ${p => p.theme.colors.dark} 0px -1px 0px,
+    ${p => p.theme.colors.dark} -1px 0px 0px;
+`;
+
+export const InfoParams = styled.p`
+  font-family: ${p => p.theme.fonts.body};
+  font-size: ${p => p.theme.fontSizes.m};
+  color: ${p => p.theme.colors.light};
+  text-shadow: ${p => p.theme.colors.grey} 1px 0px 0px,
+    ${p => p.theme.colors.dark} 0px 1px 0px,
+    ${p => p.theme.colors.dark} 0px -1px 0px,
+    ${p => p.theme.colors.dark} -1px 0px 0px;
+`;
+
+export const InfoValue = styled.span`
+  display: block;
+  padding-left: 50px;
+`;
