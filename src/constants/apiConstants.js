ec0e19391bd2aeeef0d90bf1e94ccce7bf7cf0fd
PlatinumKellerman <platinumm@ukr.net>
2023-04-19 21:35:20 +0300

    Update apiConstants.js

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -3,3 +3,4 @@ export const LANGUAGE = 'en';
 export const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 export const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
 export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+console.log(MOVIE_API_KEY, WEATHER_API_KEY);
