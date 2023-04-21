c365e3e0548bdf91528148594b84aa541822d88e
PlatinumKellerman <platinumm@ukr.net>
2023-04-22 01:51:24 +0300

    Update apiConstants.js

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -2,9 +2,8 @@ const config = {
   BASE_URL: 'https://api.themoviedb.org/3',
   LANGUAGE: 'en',
   WEATHER_BASE_URL: 'https://api.openweathermap.org/data/2.5/',
-  WEATHER_API_KEY: '',
-  REACT_APP_MOVIE_API_KEY: process.env.REACT_APP_MOVIE_API_KEY,
-  REACT_APP_WEATHER_API_KEY: process.env.REACT_APP_WEATHER_API_KEY,
+  MOVIE_API_KEY: process.env.REACT_APP_MOVIE_API_KEY,
+  WEATHER_API_KEY: process.env.REACT_APP_WEATHER_API_KEY,
 };
 
 export default config;
