45333ef034a5707783c0090abbf013bcda54d611
PlatinumKellerman <platinumm@ukr.net>
2023-05-01 21:55:29 +0300

    weather page

--- src/components/FoundCityWeather/FoundCityWeather.jsx
+++ src/components/FoundCityWeather/FoundCityWeather.jsx
@@ -1,10 +1,81 @@
+import Container from 'layout/common/Container/Container';
+import {
+  MainWrapper,
+  WeatherIcon,
+  WeatherInfoWrapper,
+  WeatherIconWrapper,
+  SecondaryWeatherInfoWrapper,
+  SunriseIcon,
+  CloudIcon,
+  SunsetIcon,
+  IconWrapper,
+  HumidityIcon,
+} from './FoundCityWeather.styled';
+
 const FoundCityWeather = ({ weather }) => {
+  const currentTime = new Date(weather.dt * 1000).toLocaleString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+  });
+  const sunriseTime = new Date(weather.sys.sunrise * 1000).toLocaleString(
+    'en-US',
+    {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: false,
+    }
+  );
+  const sunsetTime = new Date(weather.sys.sunset * 1000).toLocaleString(
+    'en-US',
+    {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: false,
+    }
+  );
+  const currentTemp = Math.round(weather.main.temp) + '°';
+  const feelTemp = Math.round(weather.main.feels_like) + '°';
+
   console.log(weather);
   return (
-    <div>
-      <p>Country:{weather.sys.country}</p>
-      <p>City name: {weather.name}</p>
-    </div>
+    <Container>
+      <MainWrapper>
+        <WeatherIconWrapper>
+          <WeatherIcon
+            src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+            alt={'clear'}
+            width="64px"
+          ></WeatherIcon>
+        </WeatherIconWrapper>
+        <WeatherInfoWrapper>
+          <p>{currentTime}</p>
+          <p>
+            {weather.name}, {weather.sys.country}
+          </p>
+          <p>
+            {`${currentTemp}`}
+            <span>{`(Feels like ${feelTemp}C)`}</span>
+          </p>
+          <p>{weather.weather[0].main}</p>
+          <IconWrapper>
+            <CloudIcon /> {`${weather.clouds.all}%`}
+          </IconWrapper>
+          <IconWrapper>
+            <HumidityIcon /> {`${weather.main.humidity}%`}
+          </IconWrapper>
+          <IconWrapper>
+            <SunriseIcon /> {sunriseTime}
+          </IconWrapper>
+          <IconWrapper>
+            <SunsetIcon /> {sunsetTime}
+          </IconWrapper>
+        </WeatherInfoWrapper>
+      </MainWrapper>
+      <SecondaryWeatherInfoWrapper></SecondaryWeatherInfoWrapper>
+    </Container>
   );
 };
 
--- src/components/FoundCityWeather/FoundCityWeather.styled.js
+++ src/components/FoundCityWeather/FoundCityWeather.styled.js
@@ -1,1 +1,61 @@
 import styled from 'styled-components';
+import { ReactComponent as Sunrise } from '../../assets/sunrise.svg';
+import { ReactComponent as Sunset } from '../../assets/sunset.svg';
+import { ReactComponent as Cloud } from '../../assets/cloud.svg';
+import { ReactComponent as Humidity } from '../../assets/humidity.svg';
+
+export const MainWrapper = styled.div`
+  width: auto;
+  display: flex;
+  background-color: ${p => p.theme.colors.muted};
+  margin: 20px;
+`;
+
+export const WeatherIconWrapper = styled.div`
+  width: auto;
+`;
+
+export const WeatherInfoWrapper = styled.div`
+  width: auto;
+`;
+
+export const WeatherIcon = styled.img`
+  width: 128px;
+`;
+
+export const SecondaryWeatherInfoWrapper = styled.div`
+  width: auto;
+`;
+
+export const SunriseIcon = styled(Sunrise)`
+  width: 48px;
+  height: 48px;
+  color: ${p => p.theme.colors.dark};
+  margin-right: 15px;
+`;
+
+export const SunsetIcon = styled(Sunset)`
+  width: 48px;
+  height: 48px;
+  color: ${p => p.theme.colors.dark};
+  margin-right: 15px;
+`;
+
+export const CloudIcon = styled(Cloud)`
+  color: ${p => p.theme.colors.dark};
+  width: 48px;
+  height: 48px;
+  margin-right: 15px;
+`;
+
+export const HumidityIcon = styled(Humidity)`
+  color: ${p => p.theme.colors.dark};
+  width: 48px;
+  height: 48px;
+  margin-right: 15px;
+`;
+
+export const IconWrapper = styled.p`
+  display: flex;
+  align-items: center;
+`;
